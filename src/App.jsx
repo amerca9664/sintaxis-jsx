eb70dfcf4cbc629c808af198653dd9d1c1463bd4
amerca9664 <amerca9664@gmail.com>
2025-05-26 21:14:05 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 const App = () => {
 	return (
-		<div>
+		<>
 			<h1>Hola mundo aaaaaa</h1>
 			<h2>No aaaaaaa</h2>
-		</div>
+		</>
 	);
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-const container = document.getElementById("root");
+import { createRoot } from 'react-dom/client';
+import App from './App';
+const container = document.getElementById('root');
 
-
-
-createRoot(container).render(<App/>);
+createRoot(container).render(<App />);
